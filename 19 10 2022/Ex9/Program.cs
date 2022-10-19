c3ebee4463222e86af6967bbab55ec1bad577966
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-10-19 23:30:17 +0700

    Ex9

---
+++ 19 10 2022/Ex9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex9 {
+	class Program {
+		static void Main(string[] args) {
+			// https://cdn.discordapp.com/attachments/1007656285853990912/1032323137855033456/unknown.png
+			Console.WriteLine("Input time (s):");
+			int seconds = int.Parse(Console.ReadLine());
+
+			int hours = seconds / (60 * 60);
+			seconds = seconds % (60 * 60);
+
+			int minutes = seconds / 60;
+			seconds = seconds % 60;
+
+			Console.WriteLine(hours + " : " + minutes + " : " + seconds);
+		}
+	}
+}

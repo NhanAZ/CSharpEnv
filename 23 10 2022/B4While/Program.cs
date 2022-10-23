348258fcc4b2cf0c81fd581e5659c03f41d6f798
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-10-23 13:06:53 +0700

    B4While

---
+++ 23 10 2022/B4While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace B4While {
+	class Program {
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Vui lòng nhập n:");
+
+			int n = int.Parse(Console.ReadLine());
+			int[] a = new int[n];
+
+			int i = 0;
+			int sum = 0;
+			while (i < n) {
+				Console.WriteLine($"Vui lòng nhập a[{i + 1}]");
+				a[i] = int.Parse(Console.ReadLine());
+				sum = sum + a[i];
+				i++;
+			}
+
+			Console.WriteLine("Sum: " + sum);
+		}
+	}
+}

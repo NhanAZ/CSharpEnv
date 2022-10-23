10a639c56ba96c0daa6f39025ee2ba8de597a7bc
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-10-24 00:45:43 +0700

    B4ForInSoChan

---
+++ 23 10 2022/B4ForInSoChan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace B4ForInSoChan {
+	class Program {
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Nhập n:");
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 1; i <= n; i++) {
+				if (i % 2 == 0) {
+					Console.Write(i + " ");
+				}
+			}
+		}
+	}
+}

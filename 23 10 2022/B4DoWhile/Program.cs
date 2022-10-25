ed69607c7b5a3487b68d80c2155116a341b69d4c
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-10-25 23:10:37 +0700

    B4DoWhile

---
+++ 23 10 2022/B4DoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace B4DoWhile {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Enter n:");
+			int n = int.Parse(Console.ReadLine());
+			int sum = 0;
+			int i = 0;
+			while (sum + i < n) {
+				sum += i;
+				i++;
+			}
+			Console.WriteLine("Sum: " + sum);
+		}
+
+	}
+}

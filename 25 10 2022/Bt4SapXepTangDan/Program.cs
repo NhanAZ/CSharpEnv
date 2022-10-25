4a55b9f99af4293c72c36a235d78b7424796437d
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-10-25 23:51:50 +0700

    Bt4SapXepTangDan

---
+++ 25 10 2022/Bt4SapXepTangDan/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bt4SapXepTangDan {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Enter a:");
+			double a = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter b:");
+			double b = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter c:");
+			double c = double.Parse(Console.ReadLine());
+
+			double temp;
+
+			if (a > b) {
+				temp = a;
+				a = b;
+				b = temp;
+			}
+
+			if (a > c) {
+				temp = a;
+				a = c;
+				c = temp;
+			}
+
+			if (b > c) {
+				temp = b;
+				b = c;
+				c = temp;
+			}
+
+			Console.WriteLine($"a({a}) < b({b}) < c({c})");
+		}
+	}
+}

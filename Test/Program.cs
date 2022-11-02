8b91244621a36775c7ac03a59e0b44a944651192
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-11-02 15:42:52 +0700

    tEST

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Test {
+	class Program {
+		static void Main(string[] args) {
+			Console.Write("Nhap vao mot so : ");
+			int number = int.Parse(Console.ReadLine());
+			int initialNumber = number;
+			int reminder, reverse = 0;
+			while (number > 0) {
+				reminder = number % 10;
+				Console.WriteLine("reminder : " + reminder);
+
+				reverse = (reverse * 10) + reminder;
+				Console.WriteLine("reverse : " + reverse);
+
+				number = number / 10;
+				Console.WriteLine("number : " + number + "\n");
+
+			}
+			Console.WriteLine($"So dao nguoc cua {initialNumber} la : {reverse}");
+			Console.ReadKey();
+
+		}
+	}
+}
